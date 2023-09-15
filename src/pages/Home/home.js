b854752aaae134a5353f55ae84a5fdbabe7b6639
DaniMendess <danimendes9728@gmail.com>
2023-09-15 00:12:03 -0300

    first commit of app lookApp

---
+++ src/pages/Home/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import { Title } from '../../styles';
+
+const Home = () => {
+  return (
+    <View>
+      <Title>OLÁ</Title>
+    </View>
+  );
+};
+
+export default Home;
---
+++ src/styles/index.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+
+export const Title = styled.Text`
+    color: red;
+    font-size: 30px;
+`;
